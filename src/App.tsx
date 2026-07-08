7c87ac123873fc59c8d03b2aed7ccec15b31e1ed
Iryna Kuptsova <irinakuptsova1708@gmail.com>
2026-07-08 17:45:47 +0200

    +

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,38 @@ import './App.css'
 
 function App() {
     return (
-        <div className="sign_in">
-            <div className="sign_in-card">
-                <label>Email</label>
-                <input
-                    type="email"
-                    placeholder="Enter your email"
-                />
-                <label>Password</label>
-                <input
-                    type="password"
-                    placeholder="********"
-                />
-                <div className="checkbox">
-                    <input type="checkbox"/>
-                    <span>Show password</span>
+        <div className="sign-in">
+            <form className="sign-in__form form">
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        className="form__input"
+                        placeholder="Enter your email"
+                    />
                 </div>
-                <button>Sign in</button>
-                <p>Don't have an account?
-                  <span>Sign up</span></p>
-            </div>
+                
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        className="form__input"
+                        placeholder="********"
+                    />
+                </div>
+
+                <label className="checkbox">
+                    <input type="checkbox" className="checkbox__input" />
+                    <span>Show password</span>
+                </label>
+
+                <button type="submit" className="btn btn-primary">Sign in</button>
+                <p className="auth-redirect">
+                    Don't have an account? <span className="higlight-tekst">Sign up</span>
+                </p>
+            </form>
         </div>
     );
 }
